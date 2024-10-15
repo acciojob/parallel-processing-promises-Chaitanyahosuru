842c31d6c8e58df5955d4b0c306b73ec5fbd36ea
Chaitanyahosuru <113969435+Chaitanyahosuru@users.noreply.github.com>
2024-10-15 20:44:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+btn.addEventListener("click", downloadImages);
+
+function downloadImages() {
+  const promises = images.map((image) => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.src = image.url;
+      img.onload = () => resolve(img);
+      img.onerror = () => reject(`Failed to load image's URL: ${image.url}`);
+    });
+  });
+
+  Promise.all(promises)
+    .then((loadedImages) => {
+      output.innerHTML = "";
+      loadedImages.forEach((img) => output.appendChild(img));
+    })
+    .catch((error) => console.error(error));
+}
+
+
